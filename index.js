8bd0057b9ae1dfd3c176000adc33120c5edd4dd0
marwa youssef <marwayoussef678@gmail.com>
2025-12-06 10:35:24 +0200

    initial folder

---
+++ index.js
@@ -0,0 +1,23 @@
+ 
+ 
+ const express = require("express");
+const dotenv= require("dotenv");
+
+
+dotenv.config();
+
+ const app = express();
+
+ const PORT = process.env.PORT || 3000 ;
+ 
+
+
+ app.get("/",(request,response)=>{
+  response.send("Welcome To Backend")
+ });
+
+
+ app.listen(PORT,function(){
+  console.log(`SERVER RUNNING @ PORT :${PORT}`)
+ });
+
